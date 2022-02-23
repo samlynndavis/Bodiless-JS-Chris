a9c9c036ed813da83daf9c5441172f649d945e0f
Chris Oden <coden@its.jnj.com>
2022-02-23 12:01:53 -0500

    Move to separate files, still working.

--- packages/__cxstarter__/src/styleguide/Page/index.tsx
+++ packages/__cxstarter__/src/styleguide/Page/index.tsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import {
-  RichTextClean, cxRichText, EditorPlainClean, cxEditorPlain
-} from '@bodiless/cx-editors';
 import { asFluidToken } from '@bodiless/cx-elements';
 import {
-  asStyleGuideTemplateToken, cxPage, cxStyleGuideTemplate, StyleGuideTemplateClean
-} from '@bodiless/cx-templates';
+  cxPage} from '@bodiless/cx-templates';
 import {
-  flowHoc, on, replaceWith
+  replaceWith
 } from '@bodiless/fclasses';
 import { __cxstarter__StyleGuideTemplate } from '../StyleGuideTemplate';
 
-const EditorPlain = on(StyleGuideTemplateClean)(
-  asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
-    Meta: flowHoc.meta.term('Token')('EditorPlain'),
-    Content: {
-      Title: replaceWith(() => <>Plain Text Editor</>),
-      Examples: on(EditorPlainClean)(cxEditorPlain.Default),
-    },
-  })
-);
-
-const RichText = on(StyleGuideTemplateClean)(
-  asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
-    Meta: flowHoc.meta.term('Token')('RichText'),
-    Content: {
-      Title: replaceWith(() => <>Rich Text Editor</>),
-      Examples: on(RichTextClean)(cxRichText.Default),
-    },
-  })
-);
-
-console.log('sgt', __cxstarter__StyleGuideTemplate);
-console.log('here now');
+import { EditorPlain, RichText } from './StyleGuideTemplate';
 
 const Default = asFluidToken({
   ...cxPage.Default,
