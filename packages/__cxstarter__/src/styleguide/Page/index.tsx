e4d18d28c10996f8eacc3760a2e2d3cf90a0d41c
Chris Oden <coden@its.jnj.com>
2022-02-23 19:08:38 -0500

    This works

--- packages/__cxstarter__/src/styleguide/Page/index.tsx
+++ packages/__cxstarter__/src/styleguide/Page/index.tsx
@@ -9,7 +9,6 @@ import {
 import {
   flowHoc, on, replaceWith
 } from '@bodiless/fclasses';
-import { __cxstarter__StyleGuideTemplate } from '../StyleGuideTemplate';
 
 const EditorPlainToken = asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
   Meta: flowHoc.meta.term('Token')('EditorPlain'),
@@ -18,19 +17,7 @@ const EditorPlainToken = asStyleGuideTemplateToken(cxStyleGuideTemplate.Default,
     Examples: on(EditorPlainClean)(cxEditorPlain.Default),
   },
 });
-
-const EditorPlainDemo = on(StyleGuideTemplateClean)(
-  EditorPlainToken
-);
-const EditorPlain = on(StyleGuideTemplateClean)(
-  asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
-    Meta: flowHoc.meta.term('Token')('EditorPlain'),
-    Content: {
-      Title: replaceWith(() => <>Plain Text Editor</>),
-      Examples: on(EditorPlainClean)(cxEditorPlain.Default),
-    },
-  })
-);
+const EditorPlainDemo = on(StyleGuideTemplateClean)(EditorPlainToken);
 
 const RichTextToken = asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
   Meta: flowHoc.meta.term('Token')('RichText'),
@@ -40,24 +27,12 @@ const RichTextToken = asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
   },
 });
 const RichTextDemo = on(StyleGuideTemplateClean)(RichTextToken);
-const RichText = on(StyleGuideTemplateClean)(
-  asStyleGuideTemplateToken(cxStyleGuideTemplate.Default, {
-    Meta: flowHoc.meta.term('Token')('RichText'),
-    Content: {
-      Title: replaceWith(() => <>Rich Text Editor</>),
-      Examples: on(RichTextClean)(cxRichText.Default),
-    },
-  })
-);
-
 
 const Default = asFluidToken({
   ...cxPage.Default,
   Components: {
     _default: replaceWith(() => <>Choose a styleguide page from the templates menu.</>),
-    // EditorPlain,
     EditorPlainDemo,
-    // RichText,
     RichTextDemo,
   },
 });
--- packages/__cxstarter__/src/styleguide/StyleGuideTemplate/index.ts
+++
@@ -1,9 +0,0 @@
-// import { RichText } from './RichText';
-import { EditorPlain } from './EditorPlain';
-import { Foo } from './Foo';
-
-export const __cxstarter__StyleGuideTemplate = {
-  // @ts-ignore
-  RichText: Foo,
-  EditorPlain,
-};
