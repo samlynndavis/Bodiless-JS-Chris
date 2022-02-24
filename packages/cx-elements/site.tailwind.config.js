30a0de094700cbfdb514d42b9453d440c81b166c
hvanyo <hvanyo@its.jnj.com>
2022-02-24 16:24:29 -0700

    Add typoprahy bottom margins & update font

--- packages/cx-elements/site.tailwind.config.js
+++ packages/cx-elements/site.tailwind.config.js
@@ -45,6 +45,9 @@ module.exports = {
       fontFamily: {
         DMSans: ['DM Sans', 'sans-serif'],
       },
+      margin: {
+        4.5: '1.125rem',
+      }
     },
   },
 
--- packages/cx-elements/src/components/Element/typography.token.ts
+++ packages/cx-elements/src/components/Element/typography.token.ts
@@ -22,30 +22,37 @@ export const cxElementTypography = asTokenGroup(meta)({
   H1: as(
     'text-m-3xl lg:text-3xl font-bold',
     cxElementColor.WithHeaderColor,
+    'mb-5 lg:mb-6',
   ),
   H2: as(
     'text-m-2xl lg:text-2xl font-bold',
     cxElementColor.WithHeaderColor,
+    'mb-5',
   ),
   H3: as(
     'text-m-xl lg:text-xl font-medium',
     cxElementColor.WithHeaderColor,
+    'mb-5',
   ),
   H4: as(
     'text-m-lg lg:text-lg font-normal',
     cxElementColor.WithHeaderColor,
+    'mb-5 lg:mb-9',
   ),
   H5: as(
     'text-m-base lg:text-base font-medium',
     cxElementColor.WithHeaderColor,
+    'mb-5 lg:mb-4.5',
   ),
   Body: as(
     'text-m-base lg:text-base font-normal',
     cxElementColor.WithBodyText,
+    'mb-6',
   ),
   Eyebrow: as(
     'text-m-xs lg:text-xs uppercase font-extrabold',
     cxElementColor.WithEyebrowColor,
+    'mb-3',
   ),
   Rest: as(
     'text-m-xs lg:text-xs font-normal',
