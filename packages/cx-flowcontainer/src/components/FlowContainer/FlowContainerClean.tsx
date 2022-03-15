be0528731707b667f6a62a590208f8739af62417
Chris Oden <coden@its.jnj.com>
2022-03-15 06:27:04 -0400

    WIP

---
+++ packages/cx-flowcontainer/src/components/FlowContainer/FlowContainerClean.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FlowContainer as FlowContainerClean } from '@bodiless/layouts-ui';
+
+const FlowContainerPreview = () => (
+  <div className="bl-bg-black">Content Region</div>
+);
+
+export default FlowContainerClean;
+
+export { FlowContainerPreview };
---
+++ packages/cx-flowcontainer/src/components/FlowContainer/index.ts
@@ -0,0 +1,2 @@
+export { FlowContainerClean, withDesignRegistryName } from './FlowContainerClean';
+export { cxFlowContainer, cxFlowContainerStatic } from './tokens';
