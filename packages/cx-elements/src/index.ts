f4b066a056f9a50a10fe88874e5bc5efc35326cd
Lucas Varela <lvarelab@its.jnj.com>
2022-03-15 13:49:57 -0300

    Remove cxElement

--- packages/cx-elements/src/index.ts
+++ packages/cx-elements/src/index.ts
@@ -16,5 +16,4 @@ export * from './components/Typography';
 export * from './components/Color';
 export * from './components/TextDecoration';
 export * from './components/FontSize';
-export * from './components/Element';
 export * from './util';
