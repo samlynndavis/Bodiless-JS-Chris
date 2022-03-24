e457b55088957fe394294f5a00a8023010ff186a
Guilherme Zeni <gzeni@its.jnj.com>
2022-03-24 11:14:26 -0300

    feature(bc-navigation): Reupdate Header After Conflicts Resolution

--- packages/cx-layout/src/components/Header/tokens/cxHeader.ts
+++ packages/cx-layout/src/components/Header/tokens/cxHeader.ts
@@ -12,35 +12,39 @@
  * limitations under the License.
  */
 
-import { withNodeKey } from '@bodiless/core';
+import {
+  withNode,
+  withNodeKey,
+} from '@bodiless/core';
 import { cxColor } from '@bodiless/cx-elements';
 import {
-  A, addClasses, flowHoc, flowIf, replaceWith, Span,
-  Ul, withDesign, withProps
-} from '@bodiless/fclasses';
+  cxBurgerMenu,
+  cxMenu,
+  cxMenuToggler,
+} from '@bodiless/cx-navigation';
 import {
-  asBodilessLink, asBodilessList, asEditable, useListContext
-} from '@bodiless/components';
-import { asHeaderToken } from '../HeaderClean';
+  Span,
+  as,
+  flowHoc,
+  replaceWith,
+  withDesign,
+  withProps,
+} from '@bodiless/fclasses';
 import { cxLogo } from '../../Logo';
-import { cxMenuToggler } from '../../MenuToggler';
 import { cxDesktopSearch, cxSearchToggler } from '../../Search';
+import { cxWhereToBuy, WhereToBuyClean } from '../../WhereToBuy';
+import { asHeaderToken } from '../HeaderClean';
 
-// @todo replace UtilityMenu placeholder tokens
-const UtilityMenuContainerPlaceholder = addClasses('flex')(Ul);
-const UtilityMenuItemPlaceholder = flowHoc(
-  asBodilessLink(),
-  addClasses('px-4 border-l-2 border-gray-400'),
-  // I'm sure there's a better way to get the first
-  // list item, but this is just a placeholder, so...
-  // Also, whoever's gonna implement this later: it's
-  // probably better to use Tailwind's `first:`, but
-  // you'll need to activate it in the config first.
-  flowIf(() => {
-    const { currentItem, items } = useListContext();
-    return Boolean(items && currentItem === items[0]);
-  })(addClasses('px-0')),
-)(A);
+// @TODO: Get rid of this after language button is implemented.
+const WithLanguageButton = flowHoc(
+  replaceWith(Span),
+  withProps({
+    children: 'Español',
+    // @TODO: Create divider tokens.
+    // @TODO: Use existing tokens.
+    className: 'text-m-base border-l-2 border-cx-primary-divider pl-5 lg:mr-4 lg:px-4 lg:border-r-2',
+  }),
+);
 
 /**
  * Token that defines a basic header.
@@ -50,39 +54,29 @@ const Base = asHeaderToken({
     MenuToggler: cxMenuToggler.Default,
     SearchToggler: cxSearchToggler.Default,
     Logo: cxLogo.Default,
-    DesktopSearch: cxDesktopSearch.Default,
-    // @todo replace UtilityMenu placeholder
-    UtilityMenu: replaceWith(flowHoc(
-      asBodilessList(),
+    Menu: cxMenu.TopNav,
+    BurgerMenu: flowHoc(
+      as(cxBurgerMenu.Default),
+      // @TODO: Is there a better way to inject WhereToBuy and (future) LanguageButton
+      // components into the menu? Maybe, move the components to another package...
       withDesign({
-        Title: flowHoc(
-          replaceWith(UtilityMenuItemPlaceholder),
-          asEditable('text', 'Menu Item')
-        )
-      })
-    )(UtilityMenuContainerPlaceholder)),
-    // @todo replace Menu placeholder
-    Menu: flowHoc(
-      replaceWith(Span),
-      withProps({
-        children: 'Main menu',
-      })
+        WhereToBuy: replaceWith(as(cxWhereToBuy.WithoutIcon)(WhereToBuyClean)),
+        // @TODO: Replace LanguageButton placeholder.
+        LanguageButton: WithLanguageButton,
+      }),
     ),
-    // @todo replace LanguageButton placeholder
-    LanguageButton: flowHoc(
-      replaceWith(Span),
-      withProps({
-        children: 'Español',
-        className: 'px-4 border-l-2 border-gray-400'
-      })
-    ),
+    DesktopSearch: cxDesktopSearch.Default,
+    UtilityMenu: cxMenu.Utility,
+    // @TODO: Replace LanguageButton placeholder.
+    LanguageButton: WithLanguageButton,
+    WhereToBuy: cxWhereToBuy.Default,
   },
   Theme: {
     Wrapper: cxColor.BgPrimaryCard,
   },
   Schema: {
     Logo: withNodeKey({ nodeKey: 'Logo' }),
-    UtilityMenu: withNodeKey({ nodeKey: 'UtilityMenu', nodeCollection: 'site' }),
+    _: withNode,
   },
   Layout: {
     Container: 'flex justify-between items-center',
@@ -94,7 +88,8 @@ const Base = asHeaderToken({
       Wrapper: 'mx-4',
     }),
     Container: 'mx-auto py-4',
-  }
+    ActionMenuContainer: 'px-4',
+  },
 });
 
 const Default = asHeaderToken({
