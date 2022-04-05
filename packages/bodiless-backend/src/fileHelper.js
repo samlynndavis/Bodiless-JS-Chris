09b5c76e42faabeadfe9e51ac8ee4c5787a89e4e
Marco Pagliarulo <mpaglia1@its.jnj.com>
2022-04-05 14:56:18 +0100

    feat(gatsby-theme-bodiless): Replace crypto-browserify with crypto-js

--- packages/bodiless-backend/src/fileHelper.js
+++ packages/bodiless-backend/src/fileHelper.js
@@ -15,7 +15,7 @@
 const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
-const crypto = require('crypto');
+const MD5 = require('crypto-js/md5');
 
 const backendStaticPath = process.env.BODILESS_BACKEND_STATIC_PATH || '';
 
@@ -27,7 +27,7 @@ const copyFilePromise = (from, to) => new Promise((resolve, reject) => {
   });
 });
 
-const generateHash = str => crypto.createHash('md5').update(str).digest('hex');
+const generateHash = str => MD5(str).toString();
 
 const isImage = fileType => {
   const imageFileTypes = [
