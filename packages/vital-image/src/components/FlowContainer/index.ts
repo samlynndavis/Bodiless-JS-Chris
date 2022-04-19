38a51ef69f87041342f73254e58151cf25774c8b
Marco Pagliarulo <mpaglia1@its.jnj.com>
2022-04-19 15:16:35 +0100

    remove useless comments

--- packages/vital-image/src/components/FlowContainer/index.ts
+++ packages/vital-image/src/components/FlowContainer/index.ts
@@ -26,15 +26,10 @@ const ImageWithNodeKey = asElementToken({
   }
 });
 
-// For the base variation, we apply the default token to the design key of
-// the designable element. This can be overridden from the design context.
 const baseVariation = {
   Image: on(Img)(ImageWithNodeKey),
 };
 
-// For variations, we apply tokens directly (not to the design key). These will
-// not be overridden from the design context. This allows a site to override
-// the base image globally, while still preserving variations in the flow container.
 const placeholderVariations = {
   Square: as(
     ImageWithNodeKey,
