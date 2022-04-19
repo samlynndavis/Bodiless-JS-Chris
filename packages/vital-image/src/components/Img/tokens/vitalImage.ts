de903537209df208211347c0bb562109864ccd4c
Chris Oden <coden@its.jnj.com>
2022-04-19 05:14:42 -0400

    Remove extraneous import.

--- packages/vital-image/src/components/Img/tokens/vitalImage.ts
+++ packages/vital-image/src/components/Img/tokens/vitalImage.ts
@@ -34,7 +34,6 @@ import {
 import { withImagePlaceholder } from '@bodiless/components';
 import { asBodilessLink, asBodilessImage } from '@bodiless/components-ui';
 import { asElementToken } from '@bodiless/vital-elements';
-import { withoutHydration } from '@bodiless/hydration';
 
 // @ts-ignore Cannot find module
 import landscapeImage from '../../../../assets/landscape_image.png';
