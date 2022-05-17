c4fad0294e1599c7fe311f08dec7ed6f4c645960
mister. jones <misterjones@users.noreply.github.com>
2022-05-17 12:08:42 -0400

    docs(bodiless-documentation): Update introductory docs (#1635)
    * Clean up and update "Intro to Bodiless Concepts" page.
    * Update tutorial to work with Minimal Starter site.
      * Original documentation relied on a `Layout` component that isn't
        available on the Minimal Starter site.
    * Add Minimal Demo (`minimal-demo`) site.
      * Minimal Demo site is a copy of the Minimal Starter site, with
        adjustments to the `package.json` file.
      * Add Gallery page to Minimal Demo site.
      * Add Gallery page created in the "Creating a Gallery Page" tutorial
        to the Minimal Demo site.
      * Link demo page and tutorial to one another.
    * Fix `withChild` bug.
    
    Co-authored-by: Chris Oden <coden@its.jnj.com>

---
+++ sites/minimal-demo/src/data/pages/gallery/Gallery.tsx
@@ -0,0 +1,68 @@
+import React, { FC, HTMLProps } from 'react';
+import {
+  H2, Section, addClasses, as, stylable, replaceWith, withMeta, withDesign,
+} from '@bodiless/fclasses';
+import { FlowContainer } from '@bodiless/layouts-ui';
+import { withNode } from '@bodiless/core';
+import CaptionedImage from './CaptionedImage';
+
+const asGalleryTile = addClasses('mx-2 border-8');
+
+const withBlueBorder = addClasses('border-blue-400');
+const withTealBorder = addClasses('border-teal-400');
+const withOrangeBorder = addClasses('border-orange-400');
+
+const design = {
+  BlueImageTile: as(
+    replaceWith(CaptionedImage),
+    asGalleryTile,
+    withBlueBorder,
+    withMeta({
+      title: 'Blue Image Tile',
+      categories: {
+        Color: ['Blue'],
+      },
+    }),
+  ),
+  TealImageTile: as(
+    replaceWith(CaptionedImage),
+    asGalleryTile,
+    withTealBorder,
+    withMeta({
+      title: 'Teal Image Tile',
+      categories: {
+        Color: ['Teal'],
+      },
+    }),
+  ),
+  OrangeImageTile: as(
+    replaceWith(CaptionedImage),
+    asGalleryTile,
+    withOrangeBorder,
+    withMeta({
+      title: 'Orange Image Tile',
+      categories: {
+        Color: ['Orange'],
+      },
+    }),
+  ),
+};
+
+const Wrapper = addClasses('my-2')(Section);
+const Header = addClasses('text-2xl')(H2);
+const Body = withDesign(design)(FlowContainer);
+
+const GalleryBase: FC<HTMLProps<HTMLDivElement>> = ({ children, ...rest }) => (
+  <Wrapper {...rest}>
+    <Header>Gallery</Header>
+    <Body nodeKey="gallery-body" />
+  </Wrapper>
+);
+
+const Gallery = as(
+  stylable,
+  withNode,
+)(GalleryBase);
+
+export const GalleryTile = asGalleryTile(CaptionedImage);
+export default Gallery;
