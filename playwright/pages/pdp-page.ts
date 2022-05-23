090381731e21108e59b715d4b947eeb1838a5a09
Maksym Omelchenko <max.omelchenko@thomascook.com>
2022-05-23 17:31:16 +0300

    feat(playwright): Migration from cypress to playwright (#1487)
    * Migration from cypress to playwright
    
    * Migration from cypress to playwright, updated
    
    * Migration from cypress to playwright, updated
    
    * typeText method was removed from pdp.spec.ts
    
    * fix: add container selectors on flow-container page
    
    * Use area-label insead of className
    
    * FlowContainer component: allow id prop
    
    * fixed all PR comments
    
    * AESQ-8348, package-lock.json updated
    
    * AESQ-8348, obsolete files were deleted
    
    * AESQ-2883, viewport removed from all files
    
    * AESQ-8348, obsolete files were deleted
    
    * Preliminary commit
    
    * AESQ-8348, fully reworked tests, 1-1 to cypress
    
    * removed obsolete files
    
    * Files updates to comply with MR comments
    
    * Fixed according to MR
    
    * Fixed according to MR
    
    * Fixed according to MR
    
    * Fixed viewport for chromium
    
    * fixed tests according to PR comments
    
    * preliminary commit
    
    * Updates according to PR comments
    
    * fix: add container selectors on flow-container page
    
    * Use area-label insead of className
    
    * FlowContainer component: allow id prop
    
    * package-lock.json updated with playwright 1.22 version
    
    * package-lock.json updated with playwright 1.22 version
    
    * changed number of commits to 1 in commit history check
    
    * limit number of workers for playwright
    
    * cards.spec.ts was excluded from test suite
    
    * fixed commit history comparison
    
    Co-authored-by: pomi <AQ1sw@DE3fr$GT5>
    Co-authored-by: tmiraniuk <tmiraniu@its.jnj.com>
    Co-authored-by: Konstantin Mishur <kmishur@its.jnj.com>
    Co-authored-by: Heidi Vanyo <58534559+hvanyo@users.noreply.github.com>

---
+++ playwright/pages/pdp-page.ts
@@ -0,0 +1,73 @@
+/**
+ * Copyright © 2021 Johnson & Johnson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// pdp-page.ts
+import { Page } from '@playwright/test';
+import { BasePage } from './base-page';
+
+export class PdpPage extends BasePage {
+  readonly page: Page;
+  readonly pagePath: string;
+  readonly pdpURL: string;
+  readonly pdpPagePath: string;
+  readonly title: string;
+  readonly accordionBody: string;
+  readonly pageIconXpath: string;
+  readonly newPageIconXpath: string;
+  readonly fieldAddPageFormXpath: string;
+  readonly checkmarkIconAddPageFormXpath: string;
+  readonly newPageLinkXpath: string;
+  readonly titleXpath: string;
+  readonly accordionOverviewBodyXpath: string;
+  readonly accordionDirectionsExpandXpath: string;
+  readonly accordionDirectionsBodyExpandedXpath: string;
+  readonly accordionDirectionsBodyPlaceholderXpath: string;
+  readonly bvTextXpath: string;
+  readonly editBVIconXpath: string;
+  readonly closeBVFormXpath: string;
+  readonly imagePlaceholderXpath: string;
+  readonly imageIconXpath: string;
+  readonly checkmarkIconImageFormXpath: string;
+  readonly flexboxXpath: string;
+  readonly addComponentIconXpath: string;
+  readonly imagePathRegex: RegExp;
+
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.pagePath = '/products/';
+    this.pdpURL = 'pdp-autotest' + Math.floor(Math.random() * 10000).toString();
+    this.pdpPagePath = this.pagePath + this.pdpURL;
+    this.title = 'AT - PDP title';
+    this.accordionBody = 'AT - Overview';
+    this.pageIconXpath = '//*[@aria-label="Page"]';
+    this.newPageIconXpath = '//*[@aria-label="New"]';
+    this.fieldAddPageFormXpath = '//*[@aria-label="Context Submenu Form"]//input[@name="new-page-path"]';
+    this.checkmarkIconAddPageFormXpath = '//*[@aria-label="Context Submenu Form"]//*[@aria-label="Submit"]';
+    this.newPageLinkXpath = '//*[@id="new-page-link"]';
+    this.titleXpath = '//*[@data-product-element="title"]//*[@data-slate-editor="true"]';
+    this.accordionOverviewBodyXpath = '//*[@data-accordion-element="accordion"][@aria-label="Overview"]//*[@data-accordion-element="accordion-body"]//*[@data-slate-editor="true"]';
+    this.accordionDirectionsExpandXpath = '//*[@data-accordion-element="accordion"][@aria-label="Directions"]//*[@data-accordion-icon="expand"]';
+    this.accordionDirectionsBodyExpandedXpath = '//*[@data-accordion-element="accordion"][@aria-label="Directions"]//*[@data-accordion-element="accordion-body"]';
+    this.accordionDirectionsBodyPlaceholderXpath = '//*[@data-accordion-element="accordion"][@aria-label="Directions"]//*[@data-accordion-element="accordion-body"]//*[text()="Enter Product Information"]';
+    this.bvTextXpath = '//*[@data-product-element="ratings-summary"][text()="Please hover and click to enter Bazaarvoice Product External ID: "]';
+    this.editBVIconXpath = '//*[@aria-label="Local Context Menu"]//*[@aria-label="Settings Reviews"]';
+    this.closeBVFormXpath = '//*[@aria-label="Context Menu Settings Reviews Form"]//*[@aria-label="Cancel"]';
+    this.imagePlaceholderXpath = '//*[@data-product-element="image"]';
+    this.imageIconXpath = '//*[@role="toolbar" and @aria-label="Local Context Menu"]//*[@aria-label="Select Image"]';
+    this.checkmarkIconImageFormXpath = '//form[@aria-label="Context Menu Select Image Form"]//button[@aria-label="Submit"]';
+    this.flexboxXpath = '//*[@data-product-element="flow-container"]';
+    this.addComponentIconXpath = '//button[@aria-label="Add Flow Container"]';
+    this.imagePathRegex = new RegExp('images/pages' + this.pdpPagePath + '/[a-zA-Z0-9]+/' + this.imageOneName, '');
+  }
+}
