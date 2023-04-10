43864c0972df4fc69db35e1acbf0686ed3d077eb
Chris Oden <coden@its.jnj.com>
2023-04-10 04:54:29 -0400

    Correct import from search.

--- packages/vital-test/src/shadow/@bodiless/vital-templates/Page.ts
+++ packages/vital-test/src/shadow/@bodiless/vital-templates/Page.ts
@@ -2,9 +2,8 @@ import vitalPage from '--vital--/lib/shadow/@bodiless/vital-templates/Page';
 import { on, as } from '@bodiless/fclasses';
 import { vitalContentListingTemplate } from '@bodiless/vital-content-listing';
 import { asFluidToken } from '@bodiless/vital-elements';
-import { vitalSearchGenericTemplate } from '@bodiless/vital-search';
 import { PDPTemplateClean, vitalPDPTemplate, GenericTemplateClean } from '@bodiless/vital-templates';
-import { withSearchMenuProvider, withSearchResult } from '@bodiless/search';
+import { vitalSearchGenericTemplate, withSearchMenuProvider, withSearchResult } from '@bodiless/vital-search';
 
 const Default = asFluidToken({
   ...vitalPage.Default,
