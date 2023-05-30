a1594fd8d9ff5ddfb1aa8f3ef637595e4ae301aa
De tarso oliveira araujo, Saulo [JANBR NON-J&J] <SdeTarso@its.jnj.com>
2023-05-30 11:29:02 -0300

    Add empty shadow function

--- packages/vital-elements/src/util/shadow.ts
+++ packages/vital-elements/src/util/shadow.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2022 Johnson & Johnson
+ * Copyright © 2023 Johnson & Johnson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,4 @@
  * limitations under the License.
  */
 
-export * from './tokenSpec';
-export * from './testing';
-export * from './shadow';
+export const shadow = (component: any, ...rest: any) => component;
