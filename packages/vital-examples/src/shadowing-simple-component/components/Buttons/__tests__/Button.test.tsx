b31b0224e083e76f0348570b808246d3e10d2991
De tarso oliveira araujo, Saulo [JANBR NON-J&J] <SdeTarso@its.jnj.com>
2023-05-31 16:39:57 -0300

    fix: Update tests snapshots

--- packages/vital-examples/src/shadowing-simple-component/components/Buttons/__tests__/Button.test.tsx
+++ packages/vital-examples/src/shadowing-simple-component/components/Buttons/__tests__/Button.test.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jest/valid-describe */
-import { Button } from '@bodiless/fclasses';
+import { ButtonClean } from '@bodiless/vital-buttons';
 import { testTokens } from '@bodiless/vital-elements';
 import { exampleButtons } from '..';
 
-describe('Buttons', testTokens(Button, exampleButtons));
+describe('Buttons', testTokens(ButtonClean, exampleButtons));
