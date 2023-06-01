ad4a2ef5fa60fcf06fd03bb65f9d8754ce7b91cd
Samuel Davis <hello@smdvs.com>
2023-06-01 11:24:44 -0500

    Fix additional linting error

--- packages/vital-examples/src/styleguide/Page/index.tsx
+++ packages/vital-examples/src/styleguide/Page/index.tsx
@@ -16,7 +16,9 @@ import { asFluidToken } from '@bodiless/vital-elements';
 import { vitalPage } from '@bodiless/vital-templates';
 import { VitalDemoStyleGuideTemplate } from './StyleGuideTemplate';
 
-const { _default, Buttons, Card, Typography } = VitalDemoStyleGuideTemplate;
+const {
+  _default, Buttons, Card, Typography
+} = VitalDemoStyleGuideTemplate;
 
 const Default = asFluidToken({
   ...vitalPage.Default,
