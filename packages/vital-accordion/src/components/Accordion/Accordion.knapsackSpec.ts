250f93eb2c4b01a78b48cc535be9cc4d6bff07c0
Ivan Rudiuk <vanchezze@gmail.com>
2023-06-15 07:26:51 -0600

    feat(knapsack): Add todo comments

--- packages/vital-accordion/src/components/Accordion/Accordion.knapsackSpec.ts
+++ packages/vital-accordion/src/components/Accordion/Accordion.knapsackSpec.ts
@@ -17,13 +17,15 @@ import AccordionClean, { AccordionComponentsStart } from './AccordionClean';
 import vitalAccordion from './tokens';
 import { AccordionComponents } from './types';
 
-// @TODO: Move to a shared Knapsack package where `VitalDesignSpec` will be.
-// Currently `@bodiless/knapsack-renderer` can't be used since it's build target is CommonJs
+// @TODO: Move to "@bodiless/vital-elements", and rename it to something like `toVitalSlotsSpec`
 export const toKnapsackSlots = (obj: Record<string, any>, baseComponentName: string = '', allowedPatternIds: string[] = []) => Object.entries(obj).reduce(
   (obj, [k]) => Object.assign(obj, {
     [k]: {
       title: `${baseComponentName}${k.replace(/([A-Z][a-z])/g, ' $1').replace(/(\d)/g, ' $1')}`,
       description: `${baseComponentName}${k.replace(/([A-Z][a-z])/g, ' $1').replace(/(\d)/g, ' $1')} Component.`,
+      /**
+       * @TODO: How do we use different `PatternIds` for different Components?
+       */
       allowedPatternIds,
     }
   }), {}
@@ -37,6 +39,11 @@ export const knapsackAccordionSpec: VitalDesignSpec<AccordionComponents> = {
   slots: toKnapsackSlots(
     AccordionComponentsStart,
     'Accordion',
+    /**
+     * @TODO: Is there a way to have these refer to the component name, or something in the spec
+     * (the compnentExportName?) and then have KS find the allowed patterns by
+     * finding any patterns which refer to those components?
+     */
     ['font-size', 'text-decoration', 'typography', 'color']
   ),
 };
